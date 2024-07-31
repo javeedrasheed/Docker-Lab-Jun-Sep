a5c6aab140e133dc696f9a1cbcf565489d88b3ee
Nabeel Hassan <thenabeelhassan@gmail.com>
2024-07-31 20:38:47 +0500

    Initialize Repository

---
+++ script.js
@@ -0,0 +1,9 @@
+let devOpsDetails = document.getElementById("devOpsDetails");
+
+function openDevOpsDetails() {
+  devOpsDetails.style.visibility = "visible";
+}
+
+let closeDevOpsDetails = () => {
+  devOpsDetails.style.visibility = "hidden";
+};
